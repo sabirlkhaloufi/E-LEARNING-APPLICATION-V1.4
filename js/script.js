e281a8001750fde99315f36fbc0affbd24aac786
sabirlkhaloufi <sabirkhaloufi@gmail.com>
2022-01-16 15:44:30 +0000

    ajouter des modification 	modifier sidebar pour responsive 	modifier le table students-list

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let menutoggle = document.querySelector(".menutoggle");
+let closemenu = document.querySelector(".close");
+let sidebar = document.querySelector(".bg-sidebar");
+let barhamburger = document.querySelector(".fa-bars");
+
+menutoggle.addEventListener("click",function(){
+    sidebar.classList.toggle("hidden-sidebar");
+    menutoggle.classList.toggle("fa-rotate-270");
+    sidebar.classList.remove("hidden-sidebar-mobile");
+   
+
+})
+
+barhamburger.addEventListener("click",function(){
+    sidebar.classList.remove("hidden-sidebar-mobile");
+})
+
+closemenu.addEventListener("click",function(){
+    sidebar.classList.add("hidden-sidebar-mobile");
+})
